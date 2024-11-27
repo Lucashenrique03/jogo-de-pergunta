8a102c14cabbfa0547b95284aaf4a8a365800e21
Lucashenrique03 <141156318+Lucashenrique03@users.noreply.github.com>
2024-11-27 11:13:05 -0300

    Add files via upload

---
+++ flashcard-lucas/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'O que foi a ditafura militar?',
+    'Ditadura é um dos regimes não democráticos ou antidemocráticos, ou seja, governos regidos por uma pessoa ou entidade política onde não há participação popular, ou em que essa participação ocorre de maneira muito restrita.'
+)
+
+criaCartao(
+    'Fisíca',
+    'O que é Espaço tempo?',
+    'O tecido espaço-tempo pode ser visualizado como uma malha tridimensional, onde o espaço representa as três dimensões espaciais (comprimento, largura e altura) e o tempo é representado como uma dimensão adicional, perpendicular às três dimensões espaciais'
+)
+
+criaCartao(
+    'Sociologia',
+    'Quem foi o maior sociologo do mundo?',
+    'Émile Durkheim'
+)
+
+criaCartao(
+    'Quimica',
+    'exemplos de crônicas?',
+    'Crônica de Machado de Assis (Gazeta de Notícias, 1889)'
+)
